9dcac0951a880df3fc7596bfd5de9fa8143a00b9
dhivya41 <123349611+dhivya41@users.noreply.github.com>
2024-03-14 15:14:35 +0530

    Update ExpenseForm.jsx

--- src/ExpenseForm.jsx
+++ src/ExpenseForm.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState } from 'react';
 
-function ExpenseForm(props) {
+function ExpenseForm({ addForm }) {
     const [expense, setExpense] = useState('');
     const [amount, setAmount] = useState(0);
     const [error, setError] = useState({});
@@ -16,7 +15,7 @@ function ExpenseForm(props) {
         setError({});
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         var err = {};
@@ -28,36 +27,49 @@ function ExpenseForm(props) {
         }
         
         setError(err);
-        console.log(err);
 
         if (Object.keys(err).length === 0) {
-            props.addForm(expense, amount);
-            setExpense('');
-            setAmount(0);
+            try {
+                const response = await fetch("/addexpense", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ title: expense, amount: amount }),
+                });
+                if (response.ok) {
+                    addForm(expense, amount);
+                    setExpense('');
+                    setAmount(0);
+                } else {
+                    const data = await response.json();
+                    console.log("Error adding expense:", data.message);
+                }
+            } catch (error) {
+                console.error("Error adding expense:", error);
+            }
         }
     };
 
     return (
         <div>
-            
             <h1 id="h1">Expense Tracker</h1>
-          <div id="main">           
-            <form onSubmit={handleSubmit}>
-                <div id="chil">
-              <div className='sp'>
-                <div> <label className='label'>Expense:</label></div>
-                <div><input className='input' type="text" placeholder='Enter the expense' value={expense} onChange={handleExpense} /> <br />
-                {error.expense} <br /></div>                
-              </div>
-              <div className='sp'>
-                <div><label className='label'>Amount:</label></div>
-                <div><input className='input' type="number" placeholder="enter the amount" value={amount} onChange={handleAmount} /> <br />
-                {error.amount} <br /></div>             
-                
-              </div>              
-                <button type="submit" id="btn">Add Expense</button>
-                </div>
-            </form>
+            <div id="main">           
+                <form onSubmit={handleSubmit}>
+                    <div id="chil">
+                        <div className='sp'>
+                            <div> <label className='label'>Expense:</label></div>
+                            <div><input className='input' type="text" placeholder='Enter the expense' value={expense} onChange={handleExpense} /> <br />
+                            {error.expense} <br /></div>                
+                        </div>
+                        <div className='sp'>
+                            <div><label className='label'>Amount:</label></div>
+                            <div><input className='input' type="number" placeholder="enter the amount" value={amount} onChange={handleAmount} /> <br />
+                            {error.amount} <br /></div>             
+                        </div>              
+                        <button type="submit" id="btn">Add Expense</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
